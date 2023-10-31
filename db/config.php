b1471fcdb378d78f5a67ba1b689c89a7e04ceb86
Samkaypro <97203039+Samkaypro@users.noreply.github.com>
2023-10-31 05:30:54 -0700

    Update config.php

--- db/config.php
+++ db/config.php
@@ -1,7 +1,7 @@
 <?php
 // Database configuration
 define("DB_HOST", "localhost");
-define("DB_NAME", "connect");
-define("DB_USER", "samkaypro1");
-define("DB_PASS", "12345678");
+define("DB_NAME", "theeroti_connect");
+define("DB_USER", "theeroti_samkaypro1");
+define("DB_PASS", "z_oK.j~j5?^d");
 ?>
